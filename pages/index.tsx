c4d4282e4a1d4274226143e4d7119387765d036a
Matt Thompson <matthew.thompson.a@gmail.com>
2019-09-06 00:35:09 -0400

    adding line to spot changes in CI/CD

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,6 +45,7 @@ const Home = () => {
       <div className="hero">
         <h1 className="title">Welcome to {appEnv}!</h1>
         <h1 className="title">TESTING THIS DEPLOY TO {appEnv} ONLY</h1>
+        <p>This is from the Testing Branch...</p>
 
         <p className="description">
           To get started, edit the <code>pages/index.js</code> or <code>pages/api/date.js</code> files, then save to
