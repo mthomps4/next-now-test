ef8776e874e96a414ff70d1ee6c5617fe4cff595
Matthew Thompson <matthew.thompson.a@gmail.com>
2019-11-15 09:48:10 -0500

    cleanup and note about staging/prod DB (#16)
    * cleanup and note about staging/prod DB
    
    * remove testing from gitlab for now...

--- pages/index.tsx
+++ pages/index.tsx
@@ -73,7 +73,10 @@ const Home = () => {
                   ))}
                 </>
               ) : (
-                <p>No Users Found.</p>
+                <div>
+                  <p>No Users Found.</p>
+                  <i>**I've removed my Postgres instnace from AWS to avoid cost.</i>
+                </div>
               )}
             </div>
           </div>
